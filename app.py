44adee66d7dc68b79a049fe2dc27415351d503ba
magicallvillage-jpg <magicallvillage@gmail.com>
2026-09-15 16:24:55 +0330

    Update app.py
    تغییر ادرس ها

--- app.py
+++ app.py
@@ -148,8 +148,8 @@ async def start(update, context):
     keyboard = [
         [InlineKeyboardButton("𝖧𝖾𝗅𝗉", callback_data="help")],
         [
-            InlineKeyboardButton("𝖴𝗉𝖽𝖺𝗍𝖾𝗌", url="https://t.me/taker_official_channel"),
-            InlineKeyboardButton("𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/takers_official_group"),
+            InlineKeyboardButton("𝖴𝗉𝖽𝖺𝗍𝖾𝗌", url="https://t.me/sogficannel"),
+            InlineKeyboardButton("𝖲𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/sogfigap"),
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
